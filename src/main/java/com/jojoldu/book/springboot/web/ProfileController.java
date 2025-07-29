5bc76eb4c79313af7e541eed77e84c0ef96ed60e
sujin kim <mh030128@naver.com>
2025-07-29 17:42:52 +0900

    20250729

--- src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -17,7 +17,7 @@ public class ProfileController {
 
     private final Environment env;
 
-    /*@GetMapping("/profile")
+    @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
@@ -30,24 +30,7 @@ public class ProfileController {
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
-                .orElse(defaultProfile);*/
-    @GetMapping("/") // <--- 이 부분을 "/" 로 변경합니다!
-    public ResponseEntity<String> profile() {
-        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"); // System.out.println 대신 log.info 사용
-        log.info("DEBUG: Root / request received! From ProfileController.");
-        log.info("active profiles: " + Arrays.toString(env.getActiveProfiles()));
-        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
-        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
-
-        String resultProfile = profiles.stream()
-                .filter(realProfiles::contains)
-                .findAny()
                 .orElse(defaultProfile);
-
-        return ResponseEntity.ok("SUCCESS: Root profile is " + resultProfile); // 반환 메시지도 변경
 
     }
 }
